28d9764eaac3e28616047bc1d333c33a0b4f5eb0
Terrence Ho <terrenceho.books@gmail.com>
2026-04-26 14:37:53 -0400

    Migrate to libs and services folders in monorepo

--- go.mod
+++ go.mod
@@ -1,30 +1,29 @@
 module github.com/TerrenceHo/monorepo
 
-go 1.24.0
+go 1.25.0
 
 require (
 	github.com/fsnotify/fsnotify v1.9.0
 	github.com/google/uuid v1.6.0
 	github.com/jmoiron/sqlx v1.4.0
-	github.com/labstack/echo v3.3.10+incompatible
-	github.com/labstack/echo/v4 v4.13.4
-	github.com/lib/pq v1.10.9
-	github.com/spf13/cobra v1.10.1
+	github.com/labstack/echo/v4 v4.15.1
+	github.com/lib/pq v1.12.3
+	github.com/spf13/cobra v1.10.2
 	github.com/spf13/viper v1.21.0
 	github.com/stretchr/testify v1.11.1
 	go.etcd.io/bbolt v1.4.3
-	go.uber.org/zap v1.27.0
-	golang.org/x/crypto v0.43.0
+	go.uber.org/zap v1.27.1
+	golang.org/x/crypto v0.50.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.5.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/labstack/gommon v0.5.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/mattn/go-isatty v0.0.21 // indirect
+	github.com/pelletier/go-toml/v2 v2.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sagikazarmark/locafero v0.12.0 // indirect
 	github.com/spf13/afero v1.15.0 // indirect
@@ -35,10 +34,10 @@ require (
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/net v0.46.0 // indirect
-	golang.org/x/sys v0.37.0 // indirect
-	golang.org/x/text v0.30.0 // indirect
-	golang.org/x/time v0.14.0 // indirect
+	golang.org/x/net v0.53.0 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/text v0.36.0 // indirect
+	golang.org/x/time v0.15.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ services/fastlinks/app.go
@@ -0,0 +1,185 @@
+package fastlinks
+
+import (
+	"context"
+	"embed"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"time"
+
+	server "github.com/TerrenceHo/monorepo/services/fastlinks/adapters/http"
+	"github.com/TerrenceHo/monorepo/services/fastlinks/services"
+	"github.com/TerrenceHo/monorepo/services/fastlinks/stores/bbolt"
+
+	// "github.com/TerrenceHo/monorepo/fastlinks/stores/postgresql"
+	"github.com/TerrenceHo/monorepo/libs/go/file"
+	"github.com/TerrenceHo/monorepo/libs/go/logging"
+	"github.com/TerrenceHo/monorepo/services/fastlinks/views"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"go.uber.org/zap"
+)
+
+type Config struct {
+	Hidebanner bool
+	Env        string
+	Port       string
+	Host       string
+	Storage    string
+	DB         DBConfig
+	Local      LocalConfig
+}
+
+type DBConfig struct {
+	User     string
+	Password string
+	DBName   string
+	Port     string
+	Host     string
+	SSLMode  string
+}
+
+type LocalConfig struct {
+	File string
+}
+
+//go:embed static
+var staticContent embed.FS
+
+func Start(conf Config) {
+	// Instantiate logger
+	logger, err := logging.ConfigureLogger(loggerType(conf.Env))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to instantiate logger: %v", err)
+		os.Exit(1)
+	}
+	logging.SetGlobalLogger(logger)
+
+	// Instantiate database connections and stores
+	var routesStore services.RoutesStore
+	switch conf.Storage {
+	case "db":
+		// db, err := postgresql.NewConnection(
+		// 	conf.DB.User,
+		// 	conf.DB.Password,
+		// 	conf.DB.DBName,
+		// 	conf.DB.Port,
+		// 	conf.DB.Host,
+		// 	conf.DB.SSLMode,
+		// )
+		// defer db.Close()
+		// if err != nil {
+		// 	logging.Fatal(
+		// 		"failed to connect to database",
+		// 		zap.Error(err),
+		// 	)
+		// }
+		// routesStore = postgresql.NewRoutesStore(db)
+	default:
+		err := os.MkdirAll(filepath.Dir(conf.Local.File), 0750)
+		if err != nil {
+			logging.Fatal(
+				"failed to create local bbolt database directory",
+				zap.Error(err),
+			)
+		}
+		err = file.CreateFileIfNotExists(conf.Local.File)
+		if err != nil {
+			logging.Fatal(
+				"failed to create local bbolt database file",
+				zap.Error(err),
+			)
+		}
+		db, err := bbolt.NewConnection(conf.Local.File, os.FileMode(0666))
+		defer db.Close()
+		if err != nil {
+			logging.Fatal(
+				"failed to open local bbolt database file",
+				zap.Error(err),
+			)
+		}
+		routesStore = bbolt.NewRoutesStore(db, "fastlinks-bucket")
+		// create bucket if it doesn't exist
+		if err = routesStore.Migrate(); err != nil {
+			logging.Fatal(
+				"failed to migrate/create local bbolt database bucket fastlinks-bucket",
+				zap.Error(err),
+			)
+		}
+	}
+
+	// create services
+	healthService := services.NewHealthService(routesStore)
+	routesService := services.NewRoutesService(routesStore)
+
+	// create http controllers
+	rootController := server.NewRootController(routesService, healthService)
+
+	// create views
+	rootView, err := views.NewView(staticContent, "static/*.html", "static/root/*.html")
+	if err != nil {
+		logging.Fatal("failed to initiate root templates", zap.Error(err))
+	}
+	newView, err := views.NewView(staticContent, "static/*.html", "static/new/*.html")
+	if err != nil {
+		logging.Fatal("failed to initiate new templates", zap.Error(err))
+	}
+
+	renderer := views.NewRenderer(rootView, newView)
+
+	// start servers and attach http routes to server
+	app := initServer(conf)
+	app.Renderer = renderer
+	rootController.Mount(app.Group(""))
+
+	go func() {
+		if err := app.Start(conf.Host + ":" + conf.Port); err != http.ErrServerClosed {
+			logging.Fatal("failed to start server", zap.Error(err))
+		}
+	}()
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	fmt.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := app.Shutdown(ctx); err != nil {
+		app.Logger.Fatal(err)
+	}
+	fmt.Println("Shutdown fastlinks server. Goodbye.")
+}
+
+func initServer(c Config) *echo.Echo {
+	app := echo.New()
+	app.HideBanner = true
+
+	app.Use(middleware.Gzip())
+	app.Use(middleware.Secure())
+	app.Use(middleware.RemoveTrailingSlash())
+	app.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `${time_rfc3339_nano} ${method} {id":"${id}","remote_ip":"${remote_ip}",` +
+			`"uri":"${uri}","status":${status},"latency":${latency},` +
+			`"latency_human":"${latency_human}","bytes_in":${bytes_in},` +
+			`"bytes_out":${bytes_out}}` + "\n",
+	}))
+
+	return app
+}
+
+func loggerType(env string) logging.LoggerType {
+	switch env {
+	case "dev":
+		return logging.DevLogger
+	case "prod":
+		return logging.ProdLogger
+	case "test":
+		return logging.TestLogger
+	default:
+		return logging.TestLogger
+	}
+}
